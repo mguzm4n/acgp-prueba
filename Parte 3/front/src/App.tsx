557cdca7420af0d2b8a6ed1fb7659696b81cfc39
Marcelo <marcelo.guzman.g@usach.cl>
2025-07-11 17:40:57 -0400

    refactor(ui): listing books text

--- Parte 3/front/src/App.tsx
+++ Parte 3/front/src/App.tsx
@@ -19,15 +19,16 @@ function App() {
           <BooksForm />
         </div>
       </div>
-      <div className="">
+      <div className="flex flex-col gap-4">
         <h1>Todos los libros registrados</h1>
         <div>
           {isPending && <div>
             Cargando datos...
           </div>}
           <div>
-            {data?.map(b => (<div key={b.id}>
-              { b.authorName }
+            {data?.map(b => (<div key={b.id}
+              className="text-sm font-medium text-gray-700 mb-1">
+              {b.id} - Libro "{b.title}", por el author { b.authorName }.
             </div>))}
           </div>
         </div>
